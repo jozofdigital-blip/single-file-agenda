195f3b09a9715d02772c2cd38aca67bb97cc791d
jozofdigital-blip <jozof.digital@gmail.com>
2025-10-15 10:07:30 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/single-file-agenda/', // 👈 ОБЯЗАТЕЛЬНО для GitHub Pages
+
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
